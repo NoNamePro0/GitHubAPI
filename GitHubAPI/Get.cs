d6e3ba9f6970484a9c2c7262f03ad88cc2728e67
No Name Pro <noname.pro.nn@gmail.com>
2020-08-05 13:21:05 +0200

    ✨ Get User

---
+++ GitHubAPI/Get.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using GitHubAPI.Model;
+using static GitHubAPI.Helper;
+
+namespace GitHubAPI
+{
+    public class Get
+    {
+        public static User User(string username, Access access = null)
+        {
+            if (access == null)
+            {
+                access = DefaultAccess;
+            }
+
+            string response = Helper.Http($"https://api.github.com/users/{username}", access);
+            return JsonConvert.DeserializeObject<User>(response);
+        }
+    }
+}
